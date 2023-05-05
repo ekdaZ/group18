5804e68e2e383f6638c456721890cefda99ce72e
Leonid Zhuravlev <ekda@pop-os.localdomain>
2023-05-05 12:07:16 +0100

    Connected base to urls, overview display function, timetable completed

--- functionality/urls.py
+++ functionality/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     path('timetable/', views.timetable, name = 'timetable'),
     path('', views.info, name= "info"),
-    path('create-activity/', views.new_activity, name = 'new-activity'), 
+    path('create-activity/', views.new_activity, name = 'create-activity'), 
     path('timer/<str:activity_name>/', views.timer, name = 'timer'),
+    path('overview/', views.overview, name = 'overview'),
+    path('stop/', views.timer_stop, name='timer_stop'),
 ]
